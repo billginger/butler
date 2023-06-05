e1ddb8904984030a6647baaf389329acf97e37dd
Fenggong Jiang <jfg@163.com>
2023-06-05 19:45:07 +0800

    Add form-text component

---
+++ front-end/components/form-text/form-text.js
@@ -0,0 +1,19 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  properties: {
+    label: {
+      type: String,
+      value: '名称',
+    },
+    maxlength: {
+      type: String,
+      value: '16',
+    },
+    value: {
+      type: String,
+      value: '',
+    },
+  },
+})
