133e6991f535af47362dcae885c1c0c1b0e19513
Fenggong Jiang <jfg@163.com>
2023-06-10 23:40:54 +0800

    Create mine-sort function

---
+++ back-end/functions/mine-sort/app.mjs
@@ -0,0 +1,42 @@
+import { UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import { ddbDocClient, getUser } from 'layer-ddb';
+
+const updateSort = async (user, event) => {
+  if (user.withUser) throw new Error('No permission!');
+  const { table } = event.pathParameters;
+  const tableList = ['account', 'category'];
+  const tableIndex = tableList.findIndex(value => value == table);
+  if (tableIndex == -1) throw new Error('No permission!');
+  const items = JSON.parse(event.body);
+  for (let index in items) {
+    const params = {
+      TableName: table,
+      Key: {
+        id: items[index],
+      },
+      UpdateExpression: 'set sort = :s',
+      ExpressionAttributeValues: {
+        ':s': index,
+      },
+    };
+    await ddbDocClient.send(new UpdateCommand(params));
+  }
+  return { result: 'success' };
+};
+
+export const handler = async (event) => {
+  try {
+    const user = await getUser(event);
+    const data = await updateSort(user, event);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err.message),
+    };
+  }
+};
