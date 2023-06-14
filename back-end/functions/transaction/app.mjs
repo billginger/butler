0086dd18683ed7b2dbbf7e563d5b5f2e8cd7b162
Fenggong Jiang <jfg@163.com>
2023-06-14 23:50:58 +0800

    Use TransactWriteCommand to putTransaction

--- back-end/functions/transaction/app.mjs
+++ back-end/functions/transaction/app.mjs
@@ -1,4 +1,4 @@
-import { PutCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { QueryCommand, TransactWriteCommand } from '@aws-sdk/lib-dynamodb';
 import { ddbDocClient, getUser } from 'layer-ddb';
 import { getMilliseconds } from 'layer-date';
 
@@ -53,10 +53,46 @@ const putTransaction = async (user, event, context) => {
   item.createDate = getMilliseconds(event.requestContext.timeEpoch);
   item.isDeleted = 0;
   const params = {
-    TableName: 'transaction',
-    Item: item,
+    TransactItems: [{
+      Put: {
+        TableName: 'transaction',
+        Item: item,
+      },
+    }]
   };
-  await ddbDocClient.send(new PutCommand(params));
+  if (item.accountTo) {
+    params.TransactItems.push({
+      Update: {
+        TableName: 'account',
+        Key: {
+          id: item.accountTo,
+        },
+        ConditionExpression: 'createUser = :u',
+        UpdateExpression: 'add amount :a',
+        ExpressionAttributeValues: {
+          ':u': item.ledger,
+          ':a': item.amount,
+        },
+      },
+    });
+  }
+  if (item.accountFrom) {
+    params.TransactItems.push({
+      Update: {
+        TableName: 'account',
+        Key: {
+          id: item.accountFrom,
+        },
+        ConditionExpression: 'createUser = :u',
+        UpdateExpression: 'add amount :a',
+        ExpressionAttributeValues: {
+          ':u': item.ledger,
+          ':a': -item.amount,
+        },
+      },
+    });
+  }
+  await ddbDocClient.send(new TransactWriteCommand(params));
   return { id: awsRequestId };
 };
 
