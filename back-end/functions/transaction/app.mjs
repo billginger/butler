b5804c04f82adf231ee7ab7a3575b6a56c34e19c
Fenggong Jiang <jfg@163.com>
2023-06-14 17:51:53 +0800

    Update transaction function

--- back-end/functions/transaction/app.mjs
+++ back-end/functions/transaction/app.mjs
@@ -2,22 +2,6 @@ import { PutCommand, QueryCommand } from '@aws-sdk/lib-dynamodb';
 import { ddbDocClient, getUser } from 'layer-ddb';
 import { getMilliseconds } from 'layer-date';
 
-const putTransaction = async (user, event, context) => {
-  const { awsRequestId } = context;
-  const item = JSON.parse(event.body);
-  item.id = awsRequestId;
-  item.ledger = user.withUser || user.openid;
-  item.createUser = user.openid;
-  item.createDate = getMilliseconds(event.requestContext.timeEpoch);
-  item.isDeleted = 0;
-  const params = {
-    TableName: 'transaction',
-    Item: item,
-  };
-  await ddbDocClient.send(new PutCommand(params));
-  return { id: awsRequestId };
-};
-
 const getAccounts = async (user) => {
   const createUser = user.withUser || user.openid;
   const params = {
@@ -35,10 +19,52 @@ const getAccounts = async (user) => {
   return data.Items;
 };
 
+const getCategories = async (user, event) => {
+  const createUser = user.withUser || user.openid;
+  const { direction } = event.pathParameters;
+  const params = {
+    TableName: 'category',
+    IndexName: 'userIndex',
+    KeyConditionExpression: 'createUser = :u',
+    FilterExpression: 'isHid = :h and direction = :d',
+    ExpressionAttributeValues: {
+      ':u': createUser,
+      ':h': false,
+      ':d': direction,
+    },
+    ProjectionExpression: 'id, label, sort',
+  };
+  const data = await ddbDocClient.send(new QueryCommand(params));
+  return data.Items;
+};
+
+const getOptions = async (user, event) => {
+  const accounts = await getAccounts(user);
+  const categories = await getCategories(user, event);
+  return { accounts, categories };
+};
+
+const putTransaction = async (user, event, context) => {
+  const { awsRequestId } = context;
+  const item = JSON.parse(event.body);
+  item.id = awsRequestId;
+  item.ledger = user.withUser || user.openid;
+  item.createUser = user.openid;
+  item.createDate = getMilliseconds(event.requestContext.timeEpoch);
+  item.isDeleted = 0;
+  const params = {
+    TableName: 'transaction',
+    Item: item,
+  };
+  await ddbDocClient.send(new PutCommand(params));
+  return { id: awsRequestId };
+};
+
 const getData = (user, event, context) => {
   switch (event.routeKey) {
+    case 'GET /transaction/accounts': return getAccounts(user);
+    case 'GET /transaction/options/{direction}': return getOptions(user, event);
     case 'PUT /transactions': return putTransaction(user, event, context);
-    case 'GET /transactions': return getAccounts(user);
   }
 };
 
