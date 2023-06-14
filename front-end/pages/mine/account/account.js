bd074273e37c99bf821078b78c7863855ca41d67
Fenggong Jiang <jfg@163.com>
2023-06-14 20:16:37 +0800

    Extraction of public function

--- front-end/pages/mine/account/account.js
+++ front-end/pages/mine/account/account.js
@@ -1,30 +1,5 @@
 import { getData } from '~/libs/get-data'
-import { twoDecimals } from '~/utils/number'
-
-const getAccounts = data => (
-  data.sort((a, b) => a.sort - b.sort).map(item => {
-    item.balance = item.currency + twoDecimals(item.amount)
-    return item
-  })
-)
-
-const getTotal = (data, showHid) => {
-  let total = []
-  data.forEach(item => {
-    if (!item.isHid || showHid) {
-      const index = total.findIndex(element => element.currency == item.currency)
-      if (index == -1) {
-        total.push({
-          currency: item.currency,
-          amount: item.amount,
-        })
-      } else {
-        total[index].amount += item.amount
-      }
-    }
-  })
-  return total.map(item => item.currency + twoDecimals(item.amount))
-}
+import { getAccounts, getTotal } from '~/libs/get-accounts'
 
 Page({
   data: {
