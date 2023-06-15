d450ca078f2ffa34354cd4cdff3a066941b1f3b1
Fenggong Jiang <jfg@163.com>
2023-06-15 21:33:40 +0800

    Create transaction payment page

---
+++ front-end/pages/transaction/payment/payment.js
@@ -0,0 +1,30 @@
+import { getData } from '~/libs/get-data'
+import { putData } from '~/libs/put-data'
+import { defaultData, getOptions, processData } from '~/libs/transaction'
+
+Page({
+  data: defaultData(),
+  onLoad() {
+    getData('/transaction/options/2', data => {
+      wx.stopPullDownRefresh()
+      this.setData({
+        accounts: getOptions(data.accounts),
+        categories: getOptions(data.categories),
+      })
+    })
+  },
+  onPullDownRefresh() {
+    this.onLoad()
+  },
+  submit() {
+    const data = processData(this.data, 2)
+    if (!data) return
+    this.setData({
+      loading: true,
+    })
+    putData('/transactions', data, () => {
+      const resetData = defaultData('basic')
+      this.setData(resetData)
+    })
+  },
+})
