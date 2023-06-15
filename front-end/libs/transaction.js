a40469a45e9b6285bfb72252e55794c34e803fce
Fenggong Jiang <jfg@163.com>
2023-06-15 20:58:13 +0800

    Extraction of common functions

---
+++ front-end/libs/transaction.js
@@ -0,0 +1,65 @@
+const defaultData = field => {
+  const basic = {
+    timeString: '',
+    summary: '',
+    amountString: '',
+    accountFromIndex: 0,
+    accountToIndex: 0,
+    categoryIndex: 0,
+    loading: false,
+  }
+  if (field == 'basic') return basic
+  const advanced = {
+    accounts: [],
+    categories: [],
+  }
+  return { ...basic, ...advanced }
+}
+
+const getOptions = data => {
+  const defaultOption = [{ label: '' }]
+  const options = data.sort((a, b) => a.sort - b.sort)
+  return [...defaultOption, ...options]
+}
+
+const processData = (data, direction) => {
+  const { timeString, summary, amountString, accountFromIndex, accountToIndex, categoryIndex } = data
+  const checkList = [
+    { label: '日期', value: timeString.length },
+    { label: '摘要', value: summary.length },
+    { label: '金额', value: amountString.length },
+    { label: '账户', value: accountFromIndex },
+    { label: '分类', value: categoryIndex },
+  ]
+  if (direction == 1) {
+    checkList.splice(3, 1, { label: '账户', value: accountToIndex })
+  }
+  if (direction == 3) {
+    const accountFrom = { label: '转出账户', value: accountFromIndex }
+    const accountTo = { label: '转入账户', value: accountToIndex }
+    checkList.splice(3, 1, accountFrom, accountTo)
+  }
+  for (const item of checkList) {
+    if (!item.value) {
+      wx.showToast({
+        title: `${item.label}不能为空！`,
+        icon: 'none',
+      })
+      return false
+    }
+  }
+  const { accounts, categories } = data
+  const timeEpoch = new Date(timeString).getTime()
+  const amount = amountString * 1
+  const category = categories[categoryIndex].id
+  const newData = { timeEpoch, summary, amount, category }
+  if (direction != 1) {
+    newData.accountFrom = accounts[accountFromIndex].id
+  }
+  if (direction != 2) {
+    newData.accountTo = accounts[accountToIndex].id
+  }
+  return newData
+}
+
+export { defaultData, getOptions, processData }
--- front-end/pages/transaction/collect/collect.js
+++ front-end/pages/transaction/collect/collect.js
@@ -1,23 +1,9 @@
 import { getData } from '~/libs/get-data'
 import { putData } from '~/libs/put-data'
-
-const getOptions = data => {
-  const defaultOption = [{ label: '' }]
-  const options = data.sort((a, b) => a.sort - b.sort)
-  return [...defaultOption, ...options]
-}
+import { defaultData, getOptions, processData } from '~/libs/transaction'
 
 Page({
-  data: {
-    accounts: [],
-    categories: [],
-    timeString: '',
-    summary: '',
-    amountString: '',
-    accountIndex: 0,
-    categoryIndex: 0,
-    loading: false,
-  },
+  data: defaultData(),
   onLoad() {
     getData('/transaction/options/1', data => {
       wx.stopPullDownRefresh()
@@ -31,40 +17,14 @@ Page({
     this.onLoad()
   },
   submit() {
-    const { timeString, summary, amountString, accountIndex, categoryIndex } = this.data
-    const checkList = [
-      { label: '日期', value: timeString.length },
-      { label: '摘要', value: summary.length },
-      { label: '金额', value: amountString.length },
-      { label: '账户', value: accountIndex },
-      { label: '分类', value: categoryIndex },
-    ]
-    for (const item of checkList) {
-      if (!item.value) {
-        return wx.showToast({
-          title: `${item.label}不能为空！`,
-          icon: 'none',
-        })
-      }
-    }
-    const { accounts, categories } = this.data
-    const timeEpoch = new Date(timeString).getTime()
-    const amount = amountString * 1
-    const accountTo = accounts[accountIndex].id
-    const category = categories[categoryIndex].id
-    const data = { timeEpoch, summary, amount, accountTo, category }
+    const data = processData(this.data, 1)
+    if (!data) return
     this.setData({
       loading: true,
     })
     putData('/transactions', data, () => {
-      this.setData({
-        timeString: '',
-        summary: '',
-        amountString: '',
-        accountIndex: 0,
-        categoryIndex: 0,
-        loading: false,
-      })
+      const resetData = defaultData('basic')
+      this.setData(resetData)
     })
   },
 })
