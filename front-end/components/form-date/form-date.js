4cc52fe882af9c21bdbbcf21d109fec6269c3535
Fenggong Jiang <jfg@163.com>
2023-06-15 14:03:25 +0800

    Create form-date component

---
+++ front-end/components/form-date/form-date.js
@@ -0,0 +1,15 @@
+Component({
+  options: {
+    addGlobalClass: true,
+  },
+  properties: {
+    label: {
+      type: String,
+      value: '日期',
+    },
+    value: {
+      type: String,
+      value: '',
+    },
+  },
+})
