de62ea8b9836f06ba80893c3629430b3ebe8cc5b
Fenggong Jiang <jfg@163.com>
2023-06-17 10:52:30 +0800

    Update form-data component

--- front-end/components/form-date/form-date.js
+++ front-end/components/form-date/form-date.js
@@ -7,6 +7,14 @@ Component({
       type: String,
       value: '日期',
     },
+    start: {
+      type: String,
+      value: '',
+    },
+    end: {
+      type: String,
+      value: '',
+    },
     value: {
       type: String,
       value: '',
