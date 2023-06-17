54eb7803a990654e42900ef2a4941c5be18f79bd
Fenggong Jiang <jfg@163.com>
2023-06-17 16:50:51 +0800

    Handle transactions query

--- back-end/functions/transaction/app.mjs
+++ back-end/functions/transaction/app.mjs
@@ -1,4 +1,4 @@
-import { TransactWriteCommand } from '@aws-sdk/lib-dynamodb';
+import { TransactWriteCommand, QueryCommand, BatchGetCommand } from '@aws-sdk/lib-dynamodb';
 import { ddbDocClient, getUser, getAccounts, getCategories } from 'layer-ddb';
 import { getMilliseconds } from 'layer-date';
 
@@ -61,10 +61,70 @@ const putTransaction = async (user, event, context) => {
   return { id: awsRequestId };
 };
 
+const processTransactions = async (items) => {
+  const transactionsUsers = items.map(item => item.createUser);
+  const Keys = [...new Set(transactionsUsers)].map(openid => ({ openid }));
+  const params = {
+    RequestItems: {
+      user: { Keys },
+    },
+  };
+  const data = await ddbDocClient.send(new BatchGetCommand(params));
+  const users = data.Responses.user;
+  const transactions = items.map(item => {
+    item.createUser = users.find(element => element.openid == item.createUser).nickname;
+    return item;
+  });
+  return transactions;
+}
+
+const queryTransactions = async (user, event) => {
+  const ledger = user.withUser || user.openid;
+  const item = JSON.parse(event.body);
+  let FilterExpression = 'contains(summary, :s)';
+  switch (item.direction) {
+    case 1:
+      FilterExpression += 'and attribute_not_exists(accountFrom) and attribute_exists(accountTo)';
+      break;
+    case 2:
+      FilterExpression += 'and attribute_exists(accountFrom) and attribute_not_exists(accountTo)';
+      break;
+    case 3:
+      FilterExpression += 'and attribute_exists(accountFrom) and attribute_exists(accountTo)';
+      break;
+  }
+  if (item.account) {
+    FilterExpression += 'and accountFrom = :a or accountTo = :a';
+  }
+  if (item.category) {
+    FilterExpression += 'and category = :c';
+  }
+  const params = {
+    TableName: 'transaction',
+    IndexName: 'transactionLedgerIndex',
+    KeyConditionExpression: 'ledger = :l and timeEpoch between :f and :t',
+    FilterExpression,
+    ExpressionAttributeValues: {
+      ':l': ledger,
+      ':f': item.timeFrom,
+      ':t': item.timeTo,
+      ':s': item.summary,
+      ':a': item.account,
+      ':c': item.category,
+    },
+    ScanIndexForward: false,
+    ProjectionExpression: 'timeEpoch, summary, amount, accountFrom, accountTo, category, createUser, createDate',
+  };
+  const data = await ddbDocClient.send(new QueryCommand(params));
+  const transactions = await processTransactions(data.Items);
+  return transactions;
+};
+
 const getData = (user, event, context) => {
   switch (event.routeKey) {
-    case 'GET /transaction/options': return getOptions(user, event);
+    case 'GET /transaction/options': return getOptions(user);
     case 'PUT /transactions': return putTransaction(user, event, context);
+    case 'POST /transactions': return queryTransactions(user, event);
   }
 };
 
